eed4ffcea874cd8c3232b3341aa2e3d524b67f02
Viktor Farcic <viktor@farcic.com>
2021-12-04 00:27:56 +0100

    Initial commit

--- ci.js
+++ ci.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: "30s", target: 25 },
+    { duration: "30s", target: 10 },
     { duration: "1m", target: 50 },
     { duration: "20s", target: 0 },
   ],
@@ -12,7 +12,7 @@ export const options = {
     "http_req_duration{what:home}": [{
       thresholds: "p(95)<100",
       abortOnFail: true,
-      delayAbortEval: "5s",
+      delayAbortEval: "10s",
     }],
   }
 };
